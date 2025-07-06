2d15d028a9fe4cfe8f4271a175d7ecd9ce2b8548
KiNG PUBBA <101438644+king1pubba@users.noreply.github.com>
2025-07-07 00:01:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const { sms, downloadMediaMessage } = require('./lib/msg');
 const { cmd } = require('./command');
 
 const prefix = '.';
-const ownerNumber = ['94704638406']; // ඔයාගේ no එක දාන්න 🙄
+const ownerNumber = ['94776290170']; // 
 
 async function connectToWhatsApp() {
     
@@ -87,10 +87,10 @@ async function connectToWhatsApp() {
             console.log('🙃 Starting... ');
             console.log('👆 Running... ');
            conn.newsletterFollow("120363183696686259@newsletter") // මේ jid එක මාරු කරන්න එපා මැනිකලාහ් 😇. පහල එකට ඔයාගෙ එක දාන්න
-           conn.newsletterFollow("120363183696686259@newsletter") // මේකට ඔයාගේ channel jid එක දාන්න
+           conn.newsletterFollow("120363399998292959@newsletter") // මේකට ඔයාගේ channel jid එක දාන්න
            console.log(" CHANNEL FOLLOW ✅")
-           let up = `Queen UDMODz connected successful ✅\n\nPREFIX: ${prefix}`; // වෙනස් කරන්නහ්
-           conn.sendMessage("94704638406@s.whatsapp.net", { image: { url: `https://i.ibb.co/5xzWmSxn/20250224-094453.png` }, caption: up }) // වෙනස් කරන්නහ්
+           let up = `Queen SP BOT connected successful ✅\n\nPREFIX: ${prefix}`; // වෙනස් කරන්නහ්
+           conn.sendMessage("94776290170@s.whatsapp.net", { image: { url: `https://i.ibb.co/5xzWmSxn/20250224-094453.png` }, caption: up }) // වෙනස් කරන්නහ්
             console.log('💙 I`m online now hutto... ');
             fs.readdirSync("./plugins/").forEach((plugin) => {
                 if (path.extname(plugin).toLowerCase() == ".js") {
@@ -117,9 +117,9 @@ if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STAT
           await conn.sendMessage("status@broadcast", {
             react: { text: randomEmoji, key: mek.key },
           }, { statusJidList: [mek.key.participant] });
-          let rep = `Status seen by Queen UDMODz 💙`; // වෙනස් කරගන්නන 
+          let rep = `Status seen by Queen SP BOT 😁💗`; // වෙනස් කරගන්නන 
           const from = mek.key.remoteJid
-         // conn.sendMessage(from,{ image: { url: `https://i.ibb.co/5xzWmSxn/20250224-094453.png` }, caption: rep },{ quoted: mek });
+          conn.sendMessage(from,{ image: { url: `https://i.ibb.co/5xzWmSxn/20250224-094453.png` }, caption: rep },{ quoted: mek });
          // ඔටෝ ststus reply ඕන නම් උඩ code එක 
         } catch (error) {
           console.error('Gagal memberi reaksi ke status', error);
@@ -159,8 +159,8 @@ const reply = (teks) => {
             forwardingScore: 0,
             isForwarded: true,
             externalAdReply: {
-            // මේ ටිකම හදාගන්න 
-                title: `𝗤𝗨𝗘𝗘𝗡 𝗨𝗗𝗠𝗢𝗗𝗭`, 
+            
+                title: `𝐐𝐔𝐄𝐄𝐍 𝐒𝐏 𝐁𝐎𝐓`, 
                 body: `The Best Multi Device Whatsapp Bot.`,
                 thumbnailUrl: `https://files.catbox.moe/bf5iea.jpg`,
                 mediaType: 1,
@@ -309,7 +309,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-    res.send("hey, UDMODZ started✅");
+    res.send("hey, SP BOT started✅");
 });
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
 setTimeout(() => {
